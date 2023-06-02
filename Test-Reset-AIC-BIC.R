6e69c9ea9857950d0aaf56183c19f00148c41df3
RodrigoBeltran-econ <135297279+RodrigoBeltran-econ@users.noreply.github.com>
2023-06-02 18:39:23 -0400

    Rename RScriptBase6.R to Test-Reset-AIC-BIC.R

--- Test-Reset-AIC-BIC.R
+++ Test-Reset-AIC-BIC.R
@@ -1,4 +1,4 @@
-#Para realizar el análisis, se necesitará el siguiente paquete: 'lmtest'. En caso de no contar con el paquete, sólo debe quitar el '#' de la línea siguiente
+#Para realizar el anÃ¡lisis, se necesitarÃ¡ el siguiente paquete: 'lmtest'. En caso de no contar con el paquete, sÃ³lo debe quitar el '#' de la lÃ­nea siguiente
 #install.packages('lmtest')
 
 library(lmtest)
@@ -11,70 +11,70 @@ data$Size<-log(data$Activo)
 attach(data)
 data$Size[!is.finite(data$Size)] <- NA
 
-#Análisis estadístico descriptivo
+#AnÃ¡lisis estadÃ­stico descriptivo
 summary(data)
 
-#Análisis de correlación
-cor(data[,c("RATV","Mon_Banc","Holding","Dueño","Socio","Gerente","Exp_gte","Fui.despedido","ROAV","RATV","B.S","Activo","Size")], use="complete")
+#AnÃ¡lisis de correlaciÃ³n
+cor(data[,c("RATV","Mon_Banc","Holding","DueÃ±o","Socio","Gerente","Exp_gte","Fui.despedido","ROAV","RATV","B.S","Activo","Size")], use="complete")
 
-####Se corre la regresión on sus respectivas variables
-#Regresión para estructura de propiedad dueño
-reg1<-lm(RATV~B.S+Mon_Banc+Size+Fui.despedido+ROAV+Dueño+Holding, data=data)
+####Se corre la regresiÃ³n on sus respectivas variables
+#RegresiÃ³n para estructura de propiedad dueÃ±o
+reg1<-lm(RATV~B.S+Mon_Banc+Size+Fui.despedido+ROAV+DueÃ±o+Holding, data=data)
 summary(reg1)
 
-#Test de Ramsey con test de criterio de información
+#Test de Ramsey con test de criterio de informaciÃ³n
 resettest(reg1, power=2:3, type="regressor", data=data)
 AIC(reg1,k=2)
 BIC(reg1)
 
-#Regresión para estructura de propiedad socio
+#RegresiÃ³n para estructura de propiedad socio
 reg2<-lm(RATV~B.S+Mon_Banc+Size+Fui.despedido+ROAV+Socio+Holding, data=data)
 summary(reg2)
 
-#Test de Ramsey con test de criterio de información
+#Test de Ramsey con test de criterio de informaciÃ³n
 resettest(reg2, power=2:3, type="regressor", data=data)
 AIC(reg2,k=2)
 BIC(reg2)
 
-#Regresión para estructura de propiedad gerente
+#RegresiÃ³n para estructura de propiedad gerente
 reg3<-lm(RATV~B.S+Mon_Banc+Size+Fui.despedido+ROAV+Gerente+Holding, data=data)
 summary(reg3)
 
-#Test de Ramsey con test de criterio de información
+#Test de Ramsey con test de criterio de informaciÃ³n
 resettest(reg3, power=2:3, type="regressor", data=data)
 AIC(reg3,k=2)
 BIC(reg3)
 
 
-####Se corre la regresión quitandole las variables de estructura de propiedad y endeudamiento
+####Se corre la regresiÃ³n quitandole las variables de estructura de propiedad y endeudamiento
 reg4<-lm(RATV~Mon_Banc+Size+Fui.despedido+ROAV+Holding, data=data)
 summary(reg4)
 
-#Test de Ramsey con test de criterio de información
+#Test de Ramsey con test de criterio de informaciÃ³n
 resettest(reg4, power=2:3, type="regressor", data=data)
 AIC(reg4,k=2)
 BIC(reg4)
 
-####Se corre la regresión original agregandole la variable experiencia del gerente
-#Regresión para estructura de propiedad dueño
-reg5<-lm(RATV~B.S+Mon_Banc+Size+Fui.despedido+ROAV+Dueño+Holding+Exp_gte, data=data)
+####Se corre la regresiÃ³n original agregandole la variable experiencia del gerente
+#RegresiÃ³n para estructura de propiedad dueÃ±o
+reg5<-lm(RATV~B.S+Mon_Banc+Size+Fui.despedido+ROAV+DueÃ±o+Holding+Exp_gte, data=data)
 summary(reg5)
 
-#Test de Ramsey con test de criterio de información
+#Test de Ramsey con test de criterio de informaciÃ³n
 resettest(reg5, power=2:3, type="regressor", data=data)
 AIC(reg5,k=2)
 BIC(reg5)
 
-#Regresión para estructura de propiedad socio
+#RegresiÃ³n para estructura de propiedad socio
 reg6<-lm(RATV~B.S+Mon_Banc+Size+Fui.despedido+ROAV+Socio+Holding+Exp_gte, data=data)
 summary(reg6)
 
-#Test de Ramsey con test de criterio de información
+#Test de Ramsey con test de criterio de informaciÃ³n
 resettest(reg6, power=2:3, type="regressor", data=data)
 AIC(reg6,k=2)
 BIC(reg6)
 
-#Regresión para estructura de propiedad gerente
+#RegresiÃ³n para estructura de propiedad gerente
 reg7<-lm(RATV~B.S+Mon_Banc+Size+Fui.despedido+ROAV+Gerente+Holding+Exp_gte, data=data)
 summary(reg7)
 
